ecbeceb9159a4b99eb369a02588cdfe03fdbc32b
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-12-03 23:31:22 +0100

    17.5a

---
+++ forcontu-vue/src/components/ArticleCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="article-card">
+    <b-card
+      v-bind:title="article.title"
+      v-bind:img-src="article.img"
+      v-bind:img-alt="article.title"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2">
+      <b-card-text>
+        {{ article.body }}
+      </b-card-text>
+      <b-button href="#" variant="primary">Ver más</b-button>
+    </b-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'article-card',
+  props: ['article']
+}
+</script>
+<style scoped>
+</style>
--- forcontu-vue/src/components/Articles.vue
+++ forcontu-vue/src/components/Articles.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="articles">
     <b-button variant="success">{{ msg }}</b-button>
+    <b-card-group columns id="list">
+      <article-card v-for="article in articles"
+                    v-bind:key="article.id"
+                    v-bind:article="article"
+      ></article-card>
+    </b-card-group>
     <ul id="list">
       <li v-for="article in articles"
           v-bind:key="article.id"
@@ -11,13 +17,43 @@
   </div>
 </template>
 <script>
+import ArticleCard from '@/components/ArticleCard'
+
 const articlesList = [
-  { id: 1, title: 'Lorem ipsum dolor sit', img:
-      'https://picsum.photos/300/150/?image=20', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis vulputate dui, in ultrices lorem aliquet quis. Aliquam at laoreet nibh, quis interdum magna. Nulla volutpat neque ut fermentum posuere.' },
-  { id: 2, title: 'Morbi interdum sceler', img:
-      'https://picsum.photos/300/150/?image=21', body: 'Vestibulum lobortis ante non quam placerat pretium consectetur sit amet arcu. Vivamus eu tortor in mauris faucibus volutpat. Proin vel consectetur velit' },
-  { id: 3, title: 'Morbi interdum sceler', img:
-      'https://picsum.photos/300/150/?image=22', body: 'Integer elit lacus, imperdiet vitae auctor in, mollis sed eros. Pellentesque eget augue et neque facilisis tristique.' },
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit',
+    img: 'https://picsum.photos/300/150/?image=20',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis vulputate dui, in ultrices lorem aliquet quis. Aliquam at laoreet nibh, quis interdum magna. Nulla volutpat neque ut fermentum posuere.' },
+  {
+    id: 2,
+    title: 'Morbi interdum sceler',
+    img: 'https://picsum.photos/300/150/?image=21',
+    body: 'Vestibulum lobortis ante non quam placerat pretium consectetur sit amet arcu. Vivamus eu tortor in mauris faucibus volutpat. Proin vel consectetur velit' },
+  {
+    id: 3,
+    title: 'Morbi interdum sceler',
+    img: 'https://picsum.photos/300/150/?image=22',
+    body: 'Integer elit lacus, imperdiet vitae auctor in, mollis sed eros. Pellentesque eget augue et neque facilisis tristique.'
+  },
+  {
+    id: 4,
+    title: 'Morbi interdum sceler',
+    img: 'https://picsum.photos/300/150/?image=23',
+    body: 'Donec euismod, libero vitae tincidunt rutrum, ante justo tincidunt sapien, vitae egestas nisi libero vitae nisi.'
+  },
+  {
+    id: 5,
+    title: 'Morbi interdum sceler',
+    img: 'https://picsum.photos/300/150/?image=24',
+    body: 'Donec euismod, libero vitae tincidunt rutrum, ante justo tincidunt sapien, vitae egestas nisi libero vitae nisi.'
+  },
+  {
+    id: 6,
+    title: 'Morbi interdum sceler',
+    img: 'https://picsum.photos/300/150/?image=25',
+    body: 'Donec euismod, libero vitae tincidunt rutrum, ante justo tincidunt sapien, vitae egestas nisi libero vitae nisi.'
+  }
 ]
 export default {
   name: 'Articles',
@@ -26,6 +62,9 @@ export default {
       msg: 'Listado de artículos',
       articles: articlesList
     }
+  },
+  components: {
+    ArticleCard
   }
 }
 </script>
@@ -36,5 +75,4 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   padding: 10px;
 }
-</
 </style>
