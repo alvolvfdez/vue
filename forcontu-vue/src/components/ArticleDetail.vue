304e0d6135a1700f859963f49a979a6a6eaa481a
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-12-04 00:17:25 +0100

    18.1a

--- forcontu-vue/src/components/ArticleDetail.vue
+++ forcontu-vue/src/components/ArticleDetail.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="article-detail">
+    <p v-if="loading">Loading...</p>
+    <b-container v-else>
     <b-container>
       <b-row>
         <b-col cols="12"><h1>{{ $route.params.id }} - {{article.title}}</h1></b-col>
@@ -22,13 +24,18 @@ export default {
   data () {
     return {
       article: [],
-      siteDomain: siteDomain
+      siteDomain: siteDomain,
+      loading: false
     }
   },
   created () {
+    this.loading = true
     axios
       .get(siteDomain + 'articulo/rest/' + this.$route.params.id, restConfig)
-      .then(response => (this.article = response.data[0]))
+      .then(response => {
+        this.article = response.data[0]
+        this.loading = false
+      })
   }
 }
 </script>
