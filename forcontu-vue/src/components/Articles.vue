eba5b8e6028f2bc152bfdea86d1b7352cf74e7e8
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-12-03 23:24:56 +0100

    17.5a

---
+++ forcontu-vue/src/components/Articles.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="articles">
+    <b-button variant="success">{{ msg }}</b-button>
+    <ul id="list">
+      <li v-for="article in articles"
+          v-bind:key="article.id"
+          v-bind:article="article"
+      >{{ article.id }} - {{ article.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+const articlesList = [
+  { id: 1, title: 'Lorem ipsum dolor sit', img:
+      'https://picsum.photos/300/150/?image=20', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam iaculis vulputate dui, in ultrices lorem aliquet quis. Aliquam at laoreet nibh, quis interdum magna. Nulla volutpat neque ut fermentum posuere.' },
+  { id: 2, title: 'Morbi interdum sceler', img:
+      'https://picsum.photos/300/150/?image=21', body: 'Vestibulum lobortis ante non quam placerat pretium consectetur sit amet arcu. Vivamus eu tortor in mauris faucibus volutpat. Proin vel consectetur velit' },
+  { id: 3, title: 'Morbi interdum sceler', img:
+      'https://picsum.photos/300/150/?image=22', body: 'Integer elit lacus, imperdiet vitae auctor in, mollis sed eros. Pellentesque eget augue et neque facilisis tristique.' },
+]
+export default {
+  name: 'Articles',
+  data () {
+    return {
+      msg: 'Listado de artículos',
+      articles: articlesList
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 10px;
+}
+</
+</style>
