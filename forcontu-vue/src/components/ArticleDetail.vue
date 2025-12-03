cf746de15d97fc117fd708575cb9999da11b5c22
Álvaro Olvera Fernández <aolvera@externas.jccm.es>
2025-12-04 00:13:08 +0100

    18.1a

--- forcontu-vue/src/components/ArticleDetail.vue
+++ forcontu-vue/src/components/ArticleDetail.vue
@@ -14,6 +14,9 @@
   </div>
 </template>
 <script>
+import {restConfig, siteDomain} from '@/App'
+import axios from 'axios'
+
 const article = {
   id: 11,
   title: 'Lorem ipsum dolor sit amet',
@@ -24,8 +27,14 @@ export default {
   name: 'article-detail',
   data () {
     return {
-      article: article
+      article: [],
+      siteDomain: siteDomain
     }
+  },
+  created () {
+    axios
+      .get(siteDomain + 'articulo/rest/' + this.$route.params.id, restConfig)
+      .then(response => (this.article = response.data[0]))
   }
 }
 </script>
